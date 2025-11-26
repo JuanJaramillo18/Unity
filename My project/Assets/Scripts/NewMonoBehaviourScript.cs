f7fa3d447c8cb52d8957481d242377075bd05207
JuanJaramillo18 <juanjaramillof@uees.edu.ec>
2025-11-25 19:09:59 -0500

    Version 2
    Colisiones, sprites y script para player

---
+++ My project/Assets/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float playerJumpForce = 20f;
+    public float playerSpeed = 5f;
+    public Sprite[] mySprites;
+    private int index = 0;
+
+    private Rigidbody2D myRigidbody2D;
+    private SpriteRenderer mySpriteRenderer;
+    //public GameObject Bullet;
+    //public GameManager myGameManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(WalkCoRutine());
+        //myGameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.linearVelocity = new Vector2(myRigidbody2D.linearVelocity.x, playerJumpForce);
+        }
+        myRigidbody2D.linearVelocity = new Vector2(playerSpeed, myRigidbody2D.linearVelocity.y);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Instantiate(Bullet, transform.position, Quaternion.identity);
+        }
+    }
+
+    IEnumerator WalkCoRutine()
+    {
+        yield return new WaitForSeconds(0.05f);
+        mySpriteRenderer.sprite = mySprites[index];
+        index++;
+        if (index == 2)
+        {
+            index = 0;
+        }
+        {
+            StartCoroutine(WalkCoRutine());
+        }
+    }
+
+}
